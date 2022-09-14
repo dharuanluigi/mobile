1884b082bfeca83c671d47c1e9a3cdecc29df624
Dharuan Luigi <dharuanluigi@gmail.com>
2022-09-13 23:09:14 -0300

    Added: header component and initial screen logo

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,20 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Image } from "react-native";
 
 import { styles } from "./styles";
 
+import logoImage from "../../assets/logo-nlw-esports.png";
+import { Heading } from "../../components/Heading";
+
 export function Home() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <Image source={logoImage} style={styles.logo} />
+
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+    </View>
+  );
 }
