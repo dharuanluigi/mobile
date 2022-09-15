7f3326b28985903b49ed685e14d7675833b9fdd5
Dharuan Luigi <dharuanluigi@gmail.com>
2022-09-15 10:39:16 -0300

    Added: load games image dinamically from backend

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,22 @@
 import { View, Image, FlatList } from "react-native";
+import { useEffect, useState } from "react";
 
 import { styles } from "./styles";
 
 import logoImage from "../../assets/logo-nlw-esports.png";
 
 import { Heading } from "../../components/Heading";
-import { GameCard } from "../../components/GameCard";
-
-import { GAMES } from "../../utils/games";
+import { GameCard, GameCardProps } from "../../components/GameCard";
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect(() => {
+    fetch("http://192.168.0.104:3000/games")
+      .then((response) => response.json())
+      .then(setGames);
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image source={logoImage} style={styles.logo} />
@@ -20,7 +27,7 @@ export function Home() {
       />
 
       <FlatList
-        data={GAMES}
+        data={games}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => <GameCard data={item} />}
         horizontal
